51ec9c6c38f9ef22d78319137fa0308d134d6128
Shawna Hightower <sdhightower6@gmail.com>
2022-04-04 17:42:45 -0600

    mui installation

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+import styles from './Pagination.module.css';
 
 const Pagination = ({ containersPerPage, totalContainers, paginate }) => {
+
     const pageNumbers = [];
 
     for (let i = 1; i <= Math.ceil(totalContainers / containersPerPage); i++) {
         pageNumbers.push(i);
     }
 
-
-        return (
-            <nav>
-            <ul className="pagination">
+    return (
+        <nav>
+            <ul className={styles.pagination}>
                 {pageNumbers.map(number => (
                     <li key={number} className='page-item'>
                         <Link to="#!" className='page-link' onClick={() => paginate(number)}>
@@ -21,7 +23,7 @@ const Pagination = ({ containersPerPage, totalContainers, paginate }) => {
                 ))}
             </ul>
         </nav>
-        );
+    );
 };
 
 export default Pagination;
