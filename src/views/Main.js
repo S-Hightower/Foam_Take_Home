12d525ca78bc3da3bf2caabd4e2fef7c1b6bf4c6
Shawna Hightower <sdhightower6@gmail.com>
2022-04-06 13:33:12 -0600

    updates

--- src/views/Main.js
+++ src/views/Main.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import { Link } from 'react-router-dom';
 
-import { Pagination } from '@mui/material';
+import Pagination from '../components/Pagination';
 
 const Main = () => {
 
-    const [status, setStatus] = useState('')
     const [containers, setContainers] = useState([]);
-    ;
+    const [currentPage, setCurrentPage] = useState(1);
+    const [containersPerPage] = useState(5);
 
     useEffect(() => {
         axios.get('http://localhost:8000/')
@@ -18,6 +18,11 @@ const Main = () => {
             })
             .catch(error => console.log(error));
     }, []);
+
+    const indexOfLastContainer = currentPage * containersPerPage;
+    const indexOfFirstContainer = indexOfLastContainer - containersPerPage;
+    const currentContainers = containers.slice(indexOfFirstContainer, indexOfLastContainer)
+    const paginate = pageNumber => setCurrentPage(pageNumber)
 
     return (
         <div className='container-sm mt-3 mb-5'>
@@ -29,27 +34,10 @@ const Main = () => {
                 <h1>Unclassified Containers</h1>
             </div>
             <table className='table table-striped table-bordered border-success'>
-                {containers.map((container, index) => {
+                {currentContainers.map((container, index) => {
                     return (
                         <tbody key={index}>
                             <tr>
-                                {/* <td>
-                                        <div>
-                                            <form onClick={e => { handleSubmit(e, { status }) }}>
-                                                <div className="form-group mt-3">
-                                                    <label>Status:</label>
-                                                    <select onChange={e => { setStatus(e.target.value) }} value={status} className="form-select mb-3">
-                                                        <option>Unclassified</option>
-                                                        <option>Foaming</option>
-                                                        <option>Not Foaming</option>
-                                                    </select>
-                                                </div>
-                                                <div className="mt-3">
-                                                    <input type='submit' className="btn btn-info" value='Final Decision?' />
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </td> */}
                                 <td>
                                     <img src={container.url} width="550" height="500" alt='' />
                                 </td>
@@ -59,7 +47,7 @@ const Main = () => {
                 }
                 )}
             </table>
-            <Pagination count={11} defaultPage={1} siblingCount={0} />
+            <Pagination totalContainers={containers.length} containersPerPage={containersPerPage} paginate={paginate}/>
         </div>//container
     )
 };
